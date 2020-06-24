119035d05d1d0ce7aeab756d253ab80168ffa0f4
William Jeffcock <jeffcockw@gmail.com>
2020-06-24 23:08:05 +0100

    Migrate to JAX-RS

--- src/main/java/rip/bolt/nerve/api/APIManager.java
+++ src/main/java/rip/bolt/nerve/api/APIManager.java
@@ -3,46 +3,56 @@ package rip.bolt.nerve.api;
 import java.util.List;
 import java.util.UUID;
 
-import rip.bolt.jsonrest.Client;
-import rip.bolt.jsonrest.Endpoint;
-import rip.bolt.jsonrest.Response;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
+
 import rip.bolt.nerve.api.definitions.Punishment;
 import rip.bolt.nerve.config.AppData;
 
 public class APIManager {
 
     private Client client;
-    private Endpoint userPunishmentsEndpoint, activeUserPunishmentsEndpoint, submitUserPunishmentEndpoint;
+    private WebTarget userPunishmentsEndpoint, activeUserPunishmentsEndpoint, submitUserPunishmentEndpoint;
 
     public APIManager() {
-        client = new Client();
-        client.setAuthorisationKey(AppData.API.getKey());
+        client = ClientBuilder.newClient().register(JacksonJsonProvider.class).register(AuthorisationHeaderFilter.class);
 
-        userPunishmentsEndpoint = client.endpoint(AppData.API.getURL() + AppData.userPunishmentEndpoint);
-        activeUserPunishmentsEndpoint = userPunishmentsEndpoint.parameter("active", "true");
-        submitUserPunishmentEndpoint = client.endpoint(AppData.API.getURL() + AppData.submitUserPunishmentEndpoint);        
+        userPunishmentsEndpoint = client.target(AppData.API.getURL() + AppData.userPunishmentEndpoint);
+        activeUserPunishmentsEndpoint = userPunishmentsEndpoint.queryParam("active", "true");
+        submitUserPunishmentEndpoint = client.target(AppData.API.getURL() + AppData.submitUserPunishmentEndpoint);
     }
 
     public List<Punishment> getActiveUserPunishments(UUID uuid) {
-        Endpoint endpoint = activeUserPunishmentsEndpoint.resolve("uuid", uuid);
-        Response response = endpoint.get();
+        WebTarget endpoint = activeUserPunishmentsEndpoint.resolveTemplate("uuid", uuid);
+        Invocation.Builder builder = endpoint.request(MediaType.APPLICATION_JSON);
 
-        return response.asList(Punishment.class);
+        return builder.get(new GenericType<List<Punishment>>() {
+        });
     }
 
     public List<Punishment> getUserPunishments(UUID uuid) {
-        Endpoint endpoint = userPunishmentsEndpoint.resolve("uuid", uuid);
-        Response response = endpoint.get();
+        WebTarget endpoint = userPunishmentsEndpoint.resolveTemplate("uuid", uuid);
+        Invocation.Builder builder = endpoint.request(MediaType.APPLICATION_JSON);
 
-        return response.asList(Punishment.class);
+        return builder.get(new GenericType<List<Punishment>>() {
+        });
     }
 
     public void submitPunishment(Punishment punishment) {
-        Endpoint endpoint = submitUserPunishmentEndpoint.resolve("uuid", punishment.getPlayer());
+        WebTarget endpoint = submitUserPunishmentEndpoint.resolveTemplate("uuid", punishment.getPlayer());
+        Invocation.Builder builder = endpoint.request(MediaType.APPLICATION_JSON);
 
-        Response response = endpoint.post(punishment);
-        if (response.getStatusCode() != 200 || response.getStatusCode() != 201)
-            System.out.println("[Nerve] Error adding punishment for " + punishment.getPlayer() + " for " + punishment.getReason() + "\n" + response.as(String.class));
+        Response response = builder.post(Entity.entity(punishment, MediaType.APPLICATION_JSON));
+        if (response.getStatus() != 200 || response.getStatus() != 201)
+            System.out.println("[Nerve] Error adding punishment for " + punishment.getPlayer() + " for " + punishment.getReason() + "\n" + response.readEntity(String.class));
     }
 
 }
