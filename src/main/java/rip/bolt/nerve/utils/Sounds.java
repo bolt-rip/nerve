487fda248ff24ffba3cee43e14623efaa34af4c9
BT (calcastor/mame) <43831917+calcastor@users.noreply.github.com>
2025-04-21 14:51:04 -0700

    Reimplement playDing

--- src/main/java/rip/bolt/nerve/utils/Sounds.java
+++ src/main/java/rip/bolt/nerve/utils/Sounds.java
@@ -1,11 +1,20 @@
 package rip.bolt.nerve.utils;
 
+import com.velocitypowered.api.network.ProtocolVersion;
 import com.velocitypowered.api.proxy.Player;
+import net.kyori.adventure.key.Key;
+import net.kyori.adventure.sound.Sound;
 
 public class Sounds {
 
-    public void playDing(Player player) {
+    static final String DING_LEGACY = "random.orb";
+    static final String DING_MODERN = "entity.experience_orb.pickup";
 
+    public void playDing(Player player) {
+        if (player.getProtocolVersion().compareTo(ProtocolVersion.MINECRAFT_1_9) < 0)
+            player.playSound(Sound.sound(Key.key(DING_LEGACY), Sound.Source.MASTER, 1, 1.2f));
+        else
+            player.playSound(Sound.sound(Key.key(DING_MODERN), Sound.Source.MASTER, 1, 1.2f));
     }
 
 }
